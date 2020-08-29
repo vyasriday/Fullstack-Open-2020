964f3d69331bb0e5a79281d4f273d35b9e831d54
Hridayesh Sharma <dev.hridayesh@gmail.com>
2020-08-29 18:39:23 +0530

    refactored Content component to render individual Part component instead of using map function

--- part1/course-information/src/index.js
+++ part1/course-information/src/index.js
@@ -5,9 +5,9 @@ const Header = (props) => <h1>{props.course}</h1>;
 
 const Content = (props) => (
 	<React.Fragment>
-		{props.parts.map((part, index) => (
-			<Part key={index} part={part} />
-		))}
+		<Part part={props.parts[0]} />
+		<Part part={props.parts[1]} />
+		<Part part={props.parts[2]} />
 	</React.Fragment>
 );
 
@@ -19,7 +19,10 @@ const Part = (props) => (
 
 const Total = (props) => (
 	<p>
-		Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
+		Number of exercises{" "}
+		{props.parts[0].exercises +
+			props.parts[1].exercises +
+			props.parts[0].exercises}
 	</p>
 );
 
@@ -43,11 +46,7 @@ const App = () => {
 		<div>
 			<Header course={course} />
 			<Content parts={parts} />
-			<Total
-				exercises1={parts[0].exercises}
-				exercises2={parts[1].exercises}
-				exercises3={parts[2].exercises}
-			/>
+			<Total parts={parts} />
 		</div>
 	);
 };
