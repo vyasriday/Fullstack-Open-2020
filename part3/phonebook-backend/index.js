5a9a2b6c010d7ce57eaa96a9217b962649a6c11d
Hridayesh Sharma <dev.hridayesh@gmail.com>
2020-09-13 16:28:29 +0530

    customized morgan to print request body, phonebook backend step 8, exercise 3.8

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -3,8 +3,22 @@ const morgan = require('morgan');
 const app = express();
 
 // json body response
-app.use(morgan('tiny'));
 app.use(express.json());
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+app.use(
+	morgan(function (tokens, req, res) {
+		return [
+			tokens.method(req, res),
+			tokens.url(req, res),
+			tokens.status(req, res),
+			tokens.res(req, res, 'content-length'),
+			'-',
+			tokens['response-time'](req, res),
+			'ms',
+			tokens.body(req, res),
+		].join(' ');
+	})
+);
 
 function generateId() {
 	return Math.floor(Math.random() * 100000 + 1);
