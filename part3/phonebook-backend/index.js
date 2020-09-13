200f1687d54498028bc16cacbfb51f56e7d74b9d
Hridayesh Sharma <dev.hridayesh@gmail.com>
2020-09-13 13:59:13 +0530

    phonebook backend step 3, exercise 3.3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -29,6 +29,15 @@ app.get('/api/persons', (req, res) => {
 	res.json(persons);
 });
 
+app.get('/api/persons/:id', (req, res) => {
+	const id = Number(req.params.id);
+	const person = persons.find((person) => person.id === id);
+	if (!person) {
+		return res.status(404).send({ error: 'Not found' });
+	}
+	res.status(200).json(person);
+});
+
 app.get('/info', (req, res) => {
 	const html = `
     <p>Phonebook has info for ${persons.length} people</p>
