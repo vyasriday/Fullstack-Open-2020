50286da59c27e95cfe75d144a9640aad75e0dc65
Hridayesh Sharma <dev.hridayesh@gmail.com>
2020-09-13 13:48:40 +0530

    phonebook backend step 1, exercise 3.1

---
+++ part3/phonebook-backend/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3001;
+const persons = [
+	{
+		name: 'Hridayesh',
+		number: '12121212',
+		id: 1,
+	},
+	{
+		name: 'Harsh',
+		number: '1234567890',
+		id: 2,
+	},
+	{
+		name: 'Marry',
+		number: '121212121',
+		id: 3,
+	},
+	{
+		name: 'Dan Abramov',
+		number: '1212121212',
+		id: 4,
+	},
+];
+
+app.get('/api/persons', (req, res) => {
+	res.json(persons);
+});
+
+app.listen(PORT, () => console.log('Server listening on port ', PORT));
